7097b524d02652c2127800f6315a724567e5f820
Shahriyar Amini <shahriyar@Shahriyar-2.local>
2014-09-27 18:00:01 -0700

    Updates

--- OpenCVSwiftStitch/SwiftStitch/SwViewController.swift
+++ OpenCVSwiftStitch/SwiftStitch/SwViewController.swift
@@ -14,6 +14,8 @@ class SwViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var compareValueLabel: UILabel!
     @IBOutlet weak var compareValueTextField: UITextView!
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -45,7 +47,8 @@ class SwViewController: UIViewController, UIScrollViewDelegate {
         return CVWrapper.compareWithOpenCVImage1(image1, image2: image2, method: compareMethod)
     }
 
-    
+
+    var result: Array<Array<UIImage>> = Array<Array<UIImage>>()
     var photoDictionary = [String: [PHAsset]]()
     var manager : PHImageManager = PHImageManager.defaultManager()
     
@@ -55,15 +58,23 @@ class SwViewController: UIViewController, UIScrollViewDelegate {
     // function synchronized
     
     @IBAction func processImages(sender: UIButton) {
+        dispatch_async(dispatch_get_main_queue()) {
+            self.processImagesInBackground()
+        }
+    }
+    
+    func processImagesInBackground() {
         let fetchOptions = PHFetchOptions()
         var assets = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: fetchOptions)
         
+        println("\(assets.count)")
+        
         assets.enumerateObjectsUsingBlock ({object, index, stop in
             let object: PHAsset = object as PHAsset
-            println(object.creationDate)
+            //println(object.creationDate)
             
             var key: String = self.getDayFromTimeStamp(object.creationDate)
-            println(key)
+            //println(key)
             
             if self.photoDictionary[key] == nil {
                 self.photoDictionary[key] = [PHAsset]()
@@ -77,55 +88,97 @@ class SwViewController: UIViewController, UIScrollViewDelegate {
         
         // PHImageRequestOptions *requestOptions = [PHImageRequestOptions new];
         
+        var count: Int = 0
+        
         for (key, day) in self.photoDictionary {
-            println(key)
-            println(day)
+            count += 1
+            //println(key)
+            //println(day)
+            print("\n\(count)")
             
             if (day.count < 2) {
                 continue
             }
             
-            if (day.count == 2) {
-                var asset0: PHAsset = day[0]
-                var asset1: PHAsset = day[1]
-                
-                // <#((UIImage!, [NSObject : AnyObject]!) -> Void)!##(UIImage!, [NSObject : AnyObject]!) -> Void#>)
-                
-                /*
-                (PHImageRequestID)requestImageDataForAsset:(PHAsset *)asset
-                options:(PHImageRequestOptions *)options
-                resultHandler:(void (^)(NSData *imageData,
-                NSString *dataUTI,
-                UIImageOrientation orientation,
-                NSDictionary *info))resultHandler
-                */
-                
-                // ((_, _) -> println("closure called")
-                
-                /*
-                manager.requestImageForAsset(<#asset: PHAsset!#>, targetSize: <#CGSize#>, contentMode: <#PHImageContentMode#>, options: <#PHImageRequestOptions!#>, resultHandler: <#((UIImage!, [NSObject : AnyObject]!) -> Void)!##(UIImage!, [NSObject : AnyObject]!) -> Void#>)*/
-                
-                //(image0: UIImage, _) in println("closure called.")
-                
-                //let closure = func (image0, _){}
-                
-                // gold
-                /*
-                let dummy: PHImageRequestID = manager.requestImageForAsset(asset0, targetSize: CGSizeMake(256, 340), contentMode: PHImageContentMode.AspectFit, options: requestOptions) {(image, d) in
-                    println("closure called")
-                }*/
-                
-                let dummy: PHImageRequestID = manager.requestImageForAsset(asset0, targetSize: CGSizeMake(256, 340), contentMode: PHImageContentMode.AspectFit, options: requestOptions, resultHandler: {(image1: UIImage!, d0: [NSObject: AnyObject]!) in
-                    //println("closure called")
-                    let anotherDummy: PHImageRequestID = self.manager.requestImageForAsset(asset1, targetSize: CGSizeMake(256, 340), contentMode: PHImageContentMode.AspectFit, options: self.requestOptions, resultHandler: {(image2: UIImage!, d1: [NSObject: AnyObject]!) in
-                        println("\(CVWrapper.compareWithOpenCVImage1(image1, image2: image2, method: 1))")
+            if (day.count > 20) {
+                continue
+            }
+            
+            for (var i = 0; i < day.count; i++) {
+                for (var j = i + 1; j < day.count; j++) {
+                    var asset0: PHAsset = day[i]
+                    var asset1: PHAsset = day[j]
+                    
+                    let dummy: PHImageRequestID = manager.requestImageForAsset(asset0, targetSize: CGSizeMake(256, 340), contentMode: PHImageContentMode.AspectFit, options: requestOptions, resultHandler: {(image1: UIImage!, d0: [NSObject: AnyObject]!) in
+                        //println("closure called")
+                        let anotherDummy: PHImageRequestID = self.manager.requestImageForAsset(asset1, targetSize: CGSizeMake(256, 340), contentMode: PHImageContentMode.AspectFit, options: self.requestOptions, resultHandler: {(image2: UIImage!, d1: [NSObject: AnyObject]!) in
+                            var correlation = CVWrapper.compareWithOpenCVImage1(image1, image2: image2, method: 0)
+                            if (correlation > 0.8) {
+                                // store asset0 and asset1 in the array
+                                println("Corr: \(correlation)")
+                                var instance: Array<UIImage> = [image1, image2]
+                                self.result.append(instance)
+                                self.imageView1.image = image1
+                                self.imageView2.image = image2
+                            }
+                        })
                     })
-                })
-                
+                }
             }
             
             
+            /*
+            if (day.count == 2) {
+            var asset0: PHAsset = day[0]
+            var asset1: PHAsset = day[1]
+            
+            // <#((UIImage!, [NSObject : AnyObject]!) -> Void)!##(UIImage!, [NSObject : AnyObject]!) -> Void#>)
+            
+            /*
+            (PHImageRequestID)requestImageDataForAsset:(PHAsset *)asset
+            options:(PHImageRequestOptions *)options
+            resultHandler:(void (^)(NSData *imageData,
+            NSString *dataUTI,
+            UIImageOrientation orientation,
+            NSDictionary *info))resultHandler
+            */
+            
+            // ((_, _) -> println("closure called")
+            
+            /*
+            manager.requestImageForAsset(<#asset: PHAsset!#>, targetSize: <#CGSize#>, contentMode: <#PHImageContentMode#>, options: <#PHImageRequestOptions!#>, resultHandler: <#((UIImage!, [NSObject : AnyObject]!) -> Void)!##(UIImage!, [NSObject : AnyObject]!) -> Void#>)*/
+            
+            //(image0: UIImage, _) in println("closure called.")
+            
+            //let closure = func (image0, _){}
+            
+            // gold
+            /*
+            let dummy: PHImageRequestID = manager.requestImageForAsset(asset0, targetSize: CGSizeMake(256, 340), contentMode: PHImageContentMode.AspectFit, options: requestOptions) {(image, d) in
+            println("closure called")
+            }*/
+            
+            let dummy: PHImageRequestID = manager.requestImageForAsset(asset0, targetSize: CGSizeMake(256, 340), contentMode: PHImageContentMode.AspectFit, options: requestOptions, resultHandler: {(image1: UIImage!, d0: [NSObject: AnyObject]!) in
+            //println("closure called")
+            let anotherDummy: PHImageRequestID = self.manager.requestImageForAsset(asset1, targetSize: CGSizeMake(256, 340), contentMode: PHImageContentMode.AspectFit, options: self.requestOptions, resultHandler: {(image2: UIImage!, d1: [NSObject: AnyObject]!) in
+            var correlation = CVWrapper.compareWithOpenCVImage1(image1, image2: image2, method: 0)
+            println("\(correlation)")
+            })
+            })
+            
+            }*/
+            
         }
+        
+        /*
+        for pairs: Array<UIImage> in result {
+        println("\n\(pairs)")
+        }*/
+        
+        println("Total pictures: \(count)")
+        println("\(result[0])")
+        
+        println("Done!")
     }
     
     func getDayFromTimeStamp(date: NSDate) -> String {
